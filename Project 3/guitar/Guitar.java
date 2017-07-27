3283d4a59c2a7af8141bc53cac0f28d2d5305026
CrutchtheClutch <halogamefreak81@gmail.com>
2017-07-27 03:59:22 -0700

    Reformat java files
    java files now more accurately display purpose of the project, removed
    unnecessary getter and setter methods

--- Project 3/guitar/Guitar.java
+++ Project 3/guitar/Guitar.java
@@ -1,90 +1,58 @@
 package guitar;
 
-/*
-* File: Guitar.java
-* Author: William Crutchfield
-*/
+/**
+ * File: Guitar.java
+ * Author: William Crutchfield
+ * Date: February 21, 2016
+ * Description: Guitar Objects, parameters of guitar are displayed with a toString method
+ */
 public class Guitar {
-    //Class Variables
+
+    // Variables
     private String guitarMake = "Epiphone";
     private String guitarModel = "Les Paul Standard";
     private int numOfStrings = 6;
     private int ampVol = 0;
     private boolean tuned = false;
-    int distortionAmount = 0;
+    private int distortionAmount = 0;
     private static int numGuitars = 0;
-    
-    //Constructors
-    //Default Constructor
-    public Guitar() {
+
+    /**
+     * Default Constructor for Guitar
+     */
+    Guitar() {
         this.ampVol = 10;
         this.tuned = true;
         this.distortionAmount = 0;
         numGuitars++;
     }
-    public Guitar (int ampVol, boolean tuned, int distortionAmount) {
+
+    /**
+     * Constructor with args for Guitar
+     * @param ampVol the level of the guitar amp
+     * @param tuned boolean if guitar is tuned
+     * @param distortionAmount amount of distortion effect added to the guitar
+     */
+    Guitar(int ampVol, boolean tuned, int distortionAmount) {
         this.ampVol = ampVol;
         this.tuned = tuned;
         this.distortionAmount = distortionAmount;
         numGuitars++;
     }
-    
-    //toString Method
+
+    /**
+     * toString method that displays guitar params
+     * @return formatted list of guitar params
+     */
     @Override
     public String toString() {
-        return "Guitar Number: " + numGuitars + 
+        return "Guitar Number: " + numGuitars +
                "\nGuitar Make: " + guitarMake +
                "\nGuitar Model: " + guitarModel +
                "\nNumber Of Strings: " + numOfStrings +
                "\nAmp Volume: " + ampVol +
                "\nTuned: " + tuned +
-               "\nDistortion Amount: " + distortionAmount + "%" + 
+               "\nDistortion Amount: " + distortionAmount + "%" +
                "\n";
-    }
-    
-    //Getter Methods
-    public String getGuitarMake() {
-        return guitarMake;
-    }
-    public String getGuitarModel() {
-        return guitarModel;
-    }
-    public int getNumOfStrings() {
-        return numOfStrings;
-    }
-    public int getAmpVol() {
-        return ampVol;
-    }
-    public boolean getTuned() {
-        return tuned;
-    }
-    public int getDistortionAmount() {
-        return distortionAmount;
-    }
-    public static int getNumGuitars() {
-        return numGuitars;
-    }
-    
-    //Setter Methods
-    public void setGuitarMake(final String guitarMake) {
-        this.guitarMake = guitarMake;
-    }
-    public void setGuitarModel(final String guitarModel) {
-        this.guitarModel = guitarModel;
-    }
-    public void setNumOfStrings(final int numOfStrings) {
-        this.numOfStrings = numOfStrings;
-    }
-    public void setAmpVol(int ampVol) {
-        this.ampVol = ampVol;
-    }
-    public void setTuned(boolean tuned) {
-        this.tuned = tuned;
-    }
-    public void setDistortionAmount(int distortionAmount) {
-        this.distortionAmount = distortionAmount;
-    }
-    public void setNumGuitars(int numGuitars) {
-        Guitar.numGuitars = numGuitars;
     }
 }
--- Project 3/guitar/TestGuitar.java
+++ Project 3/guitar/TestGuitar.java
@@ -1,24 +1,32 @@
 package guitar;
 
-/*
-* File: TestGuitar.java
-* Author: William Crutchfield
-*/
+/**
+ * File: TestGuitar.java
+ * Author: William Crutchfield
+ * Date: February 21, 2016
+ * Description: Tests the Guitar.java class, creates multiple Guitar instances and displays their parameters.
+ */
 public class TestGuitar {
     public static void main(String[] args) {
-    
-    //Guitar 1
-        //Construct A Default Guitar
+
+        // guitar1 Constructor and toStringMethod
         Guitar guitar1 = new Guitar();
-        
-        //Call toString Method
         System.out.println(guitar1.toString());
-        
-    //Guitar 2
-        //Construct the Guitar
+
+        // guitar2 Constructor and toStringMethod
         Guitar guitar2 = new Guitar(4, false, 50);
-                
-        //Call toString Method
         System.out.println(guitar2.toString());
+
+        // guitar3 Constructor and toStringMethod
+        Guitar guitar3 = new Guitar(8,true,15);
+        System.out.println(guitar3.toString());
+
+        // guitar4 Constructor and toStringMethod
+        Guitar guitar4 = new Guitar(6,false,95);
+        System.out.println(guitar4.toString());
+
+        // guitar5 Constructor and toStringMethod
+        Guitar guitar5 = new Guitar(10,true,5);
+        System.out.println(guitar5.toString());
     }
 }
